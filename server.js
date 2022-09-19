829d3a53ab8a01df6ef9e32ddf097babe0dd286d
jomar <jgil@village88.comm>
2022-09-20 06:16:45 +0800

    update api

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.status(200).json({name: "server", from: "jomar"})
 })
 
